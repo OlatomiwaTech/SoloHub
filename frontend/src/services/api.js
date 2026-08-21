ff356e918d401e427096761dcaf71dbcf087fd92
OlatomiwaTech <mrcodex2012@gmail.com>
2026-08-21 12:24:56 +0100

    New change made

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+
+// To this (temporary):
+const API_URL = 'https://solohub-waqs.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
